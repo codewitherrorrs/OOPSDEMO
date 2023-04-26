4f33c3475a2b2772afd5db1714658c1a1fbebaa2
“codewitherrors” <codewitherrors1@gmail.com>
2023-04-26 08:21:38 +0530

    Add : Enumeration Example

--- OOPSDEMO/OOPSDEMO/Program.cs
+++ OOPSDEMO/OOPSDEMO/Program.cs
@@ -4,11 +4,14 @@
     {
         public static void Main(string[] args)
         {
-          /*  Classname classname = new Classname();
-            classname.method1();
-            classname.method2("Parameter Method"); */
-            Constructor construct = new Constructor();
-            Constructor constructor = new Constructor("Hii I am Parameterized Constructor");
+            /*  Classname classname = new Classname();
+              classname.method1();
+              classname.method2("Parameter Method"); 
+              Constructor construct = new Constructor();
+              Constructor constructor = new Constructor("Hii I am Parameterized Constructor");
+            */
+            //Project obj = Project.Sagar;
+            Console.WriteLine(Project.Surya);
             Console.ReadKey();
         }
     }
